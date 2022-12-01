5cf853723ed8957cb335ba8c783eaa6001d856de
Jesus Vico <jesusvico@gmail.com>
2022-12-01 22:18:18 +0100

    Update: Now using symmetric key

--- public/p2p.js
+++ public/p2p.js
@@ -1,10 +1,8 @@
-import { generateKeyPair, getPeerId, getRandomBytes, uint8ArrayToString, stringToUint8Array, signRandoms, getPeerIdFromRaw, verifySignature } from '/utils.js';
+import { generateKey, getPeerId, getKeyString, getKeyFromString, encrypt, decrypt, getRandomBytes } from '/utils.js';
 
 const WEBSOCKET_PORT = 8080;
-let peerConnection, dataChannel, remoteId;
-let keyPair, peerId;
-
-let remotePublicKey, localRandom, remoteRandom;
+let peerConnection, dataChannel, remoteKey, remotePeerId;
+let key, keyString, peerId;
 
 let wsInterval;
 let sendButton = document.getElementById('send-button');
@@ -36,7 +34,7 @@ const socketOnMessage = async ({ data }) => {
         break;
       case 'offer':
         console.log('RECEIVE ACTION: offer ' + jsonMessage.data.remoteId);
-        remoteId = jsonMessage.data.remoteId;
+        remotePeerId = jsonMessage.data.remoteId;
         delete jsonMessage.data.remoteId;
 
         // If we have a dataChannel ignore the new one
@@ -47,7 +45,7 @@ const socketOnMessage = async ({ data }) => {
         const answer = await peerConnection.createAnswer();
         await peerConnection.setLocalDescription(answer);
 
-        sendSocketMessage('answer', { remoteId, answer });
+        sendSocketMessage('answer', { remoteId: remotePeerId, answer });
         break;
       case 'answer':
         console.log('RECEIVE ACTION: answer ' + jsonMessage.data.answer);
@@ -77,11 +75,13 @@ const socketOnClose = () => {
 
 const start = async () => {
   // If there is no keyPair generate it
-  if (!keyPair) {
-    keyPair = await generateKeyPair();
-    console.log(keyPair);
-    peerId = await getPeerId(keyPair.publicKey);
+  if (!key) {
+    key = await generateKey();
+    keyString = await getKeyString(key);
+    console.log(key);
+    peerId = await getPeerId(key);
   }
+  console.log('Local Peer ID: ', peerId);
 
   socket = new WebSocket(`wss://${window.location.hostname}:${WEBSOCKET_PORT}`);
   socket.onopen = socketOnOpen;
@@ -99,14 +99,15 @@ const sendSocketMessage = (action, data) => {
 sendButton.addEventListener('click', async () => {
   sending = true;
   await start();
-  let downloadLink = document.getElementById('download-link');
-  downloadLink.href = `https://${window.location.host}/#${peerId}`;
-  downloadLink.innerHTML = `https://${window.location.host}/#${peerId}`;
+  let url = `https://${window.location.host}/#${keyString}`;
+  let openUrl = document.getElementById('open-url');
+  openUrl.onclick = () => window.open(url);
   sendButton.disabled = true;
   copyButton.style.display = 'block';
+  openUrl.style.display = 'block';
 
   document.getElementById('qrcode').innerHTML = "";
-  new QRCode(document.getElementById("qrcode"), `https://${window.location.host}/#${peerId}`);
+  new QRCode(document.getElementById("qrcode"), `https://${window.location.host}/#${keyString}`);
 });
 
 copyButton.addEventListener('click', () => {
@@ -124,18 +125,18 @@ const addMessage = (message) => {
 // ==================== P2P ====================
 const connect = async () => {
   try {
-    if (!remoteId) {
+    if (!remotePeerId) {
       alert('Please enter a remote id');
       return;
     }
 
-    console.log('CONNECTING to: ' + remoteId);
+    console.log('CONNECTING to: ' + remotePeerId);
     await initializePeerConnection();
     initializeDataChannel();
 
     const offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
-    sendSocketMessage('offer', { offer, remoteId });
+    sendSocketMessage('offer', { offer, remoteId: remotePeerId });
   } catch (error) {
     console.error('failed to initialize call', error);
   }
@@ -145,13 +146,8 @@ const closeConnection = async () => {
   console.log('Data Channel CLOSED');
 
   dataChannel = null;
-  remoteId = null;
-  remotePublicKey = null;
-  localRandom = null;
-  remoteRandom = null;
-
-  document.getElementById('connectionForm').style.display = 'flex';
-  document.getElementById('chat').style.display = 'none';
+  remotePeerId = null;
+  remoteKey = null;
 }
 
 const initializeDataChannel = () => {
@@ -169,8 +165,8 @@ const initializePeerConnection = async () => {
     if (!candidate) return;
 
     console.log('peerConnection icecandidate ' + candidate);
-    console.log('remote ' + remoteId);
-    sendSocketMessage('iceCandidate', { remoteId, candidate });
+    console.log('remote ' + remotePeerId);
+    sendSocketMessage('iceCandidate', { remoteId: remotePeerId, candidate });
   };
 
   peerConnection.oniceconnectionstatechange = () => {
@@ -193,61 +189,20 @@ const initializePeerConnection = async () => {
 const initializeDataChannelListeners = () => {
   dataChannel.onopen = async () => {
     console.log('Data Channel OPEN')
-    // If you are downloading send a random number
-    if (!sending) {
-      //let rawPublicKey = new Uint8Array(await crypto.subtle.exportKey('raw', keyPair.publicKey));
-      //dataChannel.send(JSON.stringify({ type: 'publicKey', data: uint8ArrayToString(rawPublicKey) }));
-      localRandom = await getRandomBytes(32);
-      dataChannel.send(JSON.stringify({ type: 'random', data: uint8ArrayToString(localRandom) }));
+    // If you are the uploader send the encrypted file
+    if (sending) {
+      // TODO
+      uploadFile();
     }
   }
   dataChannel.onclose = () => closeConnection();
   dataChannel.onerror = (error) => error('Data Channel error: ' + error);
   dataChannel.onmessage = async ({ data }) => {
-    let jsonData = JSON.parse(data);
+    // If we have the iv all the messages are encrypted
+    let plaintext = await decrypt(remoteKey, data);
+    let jsonData = JSON.parse(plaintext);
 
     switch (jsonData.type) {
-
-      case 'random':
-        remoteRandom = stringToUint8Array(jsonData.data);
-        if(sending) {
-          // First send random
-          localRandom = await getRandomBytes(32);
-          dataChannel.send(JSON.stringify({ type: 'random', data: uint8ArrayToString(localRandom) }));
-          // Then send the raw public key
-          let rawPublicKey = new Uint8Array(await crypto.subtle.exportKey('raw', keyPair.publicKey));
-          dataChannel.send(JSON.stringify({ type: 'publicKey', data: uint8ArrayToString(rawPublicKey) }));
-          // Then send the signature of both randoms
-          let signature = new Uint8Array(await signRandoms(keyPair.privateKey, localRandom, remoteRandom));
-          dataChannel.send(JSON.stringify({ type: 'signature', data: uint8ArrayToString(signature) }));
-          // Send the file
-          uploadFile();
-        }
-        break;
-
-      case 'publicKey':
-        // Check if the remote public key corresponds to the remotePeerId
-        console.log('Remote public key', jsonData.data);
-        remotePublicKey = stringToUint8Array(jsonData.data);
-        let remotePublicKeyId = await getPeerIdFromRaw(remotePublicKey);
-        if (remotePublicKeyId != remoteId) {
-          console.error('Invalid remote public key');
-          dataChannel.close();
-          break;
-        }
-        break;
-
-      case 'signature':
-        // Check if the remote signature is valid
-        let remoteSignature = stringToUint8Array(jsonData.data);
-        let result = await verifySignature(remotePublicKey, remoteSignature.buffer, localRandom, remoteRandom);
-        if (!result) {
-          console.error('Invalid signature')
-          dataChannel.close();
-          break;
-        }
-        console.log('Valid signature');
-        break;
 
       case 'message':
         console.log('Data Channel data: ' + jsonData.data);
@@ -265,6 +220,8 @@ const initializeDataChannelListeners = () => {
 
       case 'finish':
         await saveToDisk(downloadedMessage.join(''), fileName);
+        dataChannel.close();
+
         break;
       
       case 'status':
@@ -288,9 +245,9 @@ const uploadFile = () => {
   let reader = new FileReader();
   reader.readAsDataURL(file);
 
-  reader.onload = () => {
-    // Send file name
-    dataChannel.send(JSON.stringify({type: "name", data: file.name}));
+  reader.onload = async () => {    
+    let ciphertext = await encrypt(key, JSON.stringify({type: "name", data: file.name}));
+    dataChannel.send(ciphertext);
 
     fileData = reader.result;
     // Send file
@@ -298,8 +255,9 @@ const uploadFile = () => {
   }
 }
 
-const sendFile = () => {
-  const chunkSize = 10000;
+const sendFile = async () => {
+  const chunkSize = 1000;
+  let ciphertext;
   while(fileData.length) {
     // Wait for the buffer
     if(dataChannel.bufferedAmount > dataChannel.bufferedAmountLowThreshold) {
@@ -310,12 +268,16 @@ const sendFile = () => {
       return;
     }
     const chunk = fileData.slice(0, chunkSize);
+    console.log('Chunk: ', chunk)
     fileData = fileData.slice(chunkSize, fileData.length);
-    dataChannel.send(JSON.stringify({ type: "chunk", data: chunk }));
-    dataChannel.send(JSON.stringify({ type: "status", data: fileData.length }));
+    ciphertext = await encrypt(key, JSON.stringify({ type: "chunk", data: chunk }));
+    dataChannel.send(ciphertext);
+    ciphertext = await encrypt (key, JSON.stringify({ type: "status", data: fileData.length }));
+    dataChannel.send(ciphertext);
   }
   if(!fileData.length) {
-    dataChannel.send(JSON.stringify({ type: "finish" }));
+    ciphertext = await encrypt(key, JSON.stringify({ type: "finish" }));
+    dataChannel.send(ciphertext);
   }
 }
 
@@ -331,7 +293,13 @@ const saveToDisk = async (fileUrl, fileName) => {
 
 // Check if we are downloading
 if(window.location.hash) {
-  remoteId = window.location.hash.replace('#', '');
+  let remoteStringKey = window.location.hash.replace('#', '');
+  console.log(remoteStringKey)
+  remoteKey = await getKeyFromString(remoteStringKey);
+  remotePeerId = await getPeerId(remoteKey);
+  console.log('Remote Peer ID: ', remotePeerId);
+
   await start();
   setTimeout(() => connect(), 1000);
+
 }
--- public/utils.js
+++ public/utils.js
@@ -9,37 +9,49 @@
 
 // ==================== CRYPTO ====================
 
-export async function generateKeyPair() {
+// Get random values, returns an ArrayBuffer
+export function getRandomBytes(size) {
+  const buf = new Uint8Array(size);
+  crypto.getRandomValues(buf);
+  return buf.buffer;
+}
+
+// Generate symmetric key
+export async function generateKey() {
   const algorithm = {
-    name: 'ECDSA',
-    namedCurve: 'P-256'
+    name: 'AES-GCM',
+    length: '256'
   }
-  const keyUsages = ['sign', 'verify'];
+  const keyUsages = ['encrypt', 'decrypt'];
   const key = await crypto.subtle.generateKey(algorithm, true, keyUsages);
   
   return key;
 }
 
-export function uint8ArrayToString(u8array) {
-  let str = "";
-  u8array.forEach((value) => {
-    let hexValue = value.toString(16);
-    while (hexValue.length < 2) hexValue = '0' + hexValue;
-    str += hexValue
-  });
-  return str;
+export async function getKeyString(key) {
+  let rawKey = new Uint8Array(await crypto.subtle.exportKey('raw', key));
+  let stringRawKey = uint8ArrayToHexString(rawKey);
+  return stringRawKey;
 }
 
-export function stringToUint8Array(hexString) {
-  return Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+export async function getKeyFromString(stringKey) {
+  const algorithm = {
+    name: 'AES-GCM',
+    length: '256'
+  }
+  const keyUsages = ['encrypt', 'decrypt'];
+
+  const rawKey = hexStringToUint8Array(stringKey);
+
+  let key = await crypto.subtle.importKey('raw', rawKey, algorithm, true, keyUsages);
+  return key;
 }
 
-export async function getPeerId(publicKey) {
-  let rawPublicKey = new Uint8Array(await crypto.subtle.exportKey('raw', publicKey));
-  let stringPublicKey = uint8ArrayToString(rawPublicKey);
-  console.log('Raw local public key:', stringPublicKey);
+export async function getPeerId(key) {
+  let rawKey = new Uint8Array(await crypto.subtle.exportKey('raw', key));
+  let stringRawKey = uint8ArrayToHexString(rawKey);
 
-  return (await sha256(stringPublicKey)).substring(0, 40);
+  return (await sha256(stringRawKey)).substring(0, 40);
 }
 
 export async function sha256(message) {
@@ -57,58 +69,69 @@ export async function sha256(message) {
   return hashHex;
 }
 
+export async function encrypt(key, plaintextString) {
+  const plaintext = stringToArrayBuffer(plaintextString);
+  const iv = getRandomBytes(16);
+  console.log(iv)
 
-/* HANDSHAKE SIGNATURE */
+  const algorithm = {
+    name: 'AES-GCM',
+    iv: iv
+  }
 
-export async function getPeerIdFromRaw(rawPublicKey) {
-  let stringPublicKey = uint8ArrayToString(rawPublicKey);
-  console.log(stringPublicKey);
+  let ciphertext = await crypto.subtle.encrypt(algorithm, key, plaintext);
 
-  return (await sha256(stringPublicKey)).substring(0, 40);
+  // Concat iv and ciphertext
+  let result = new Uint8Array(iv.byteLength + ciphertext.byteLength);
+  result.set(new Uint8Array(iv), 0);
+  result.set(new Uint8Array(ciphertext), iv.byteLength);
+
+  return result;
 }
 
-export async function getRandomBytes(length) {
-  let randomBytes = new Uint8Array(length);
-  crypto.getRandomValues(randomBytes);
-  return randomBytes;
+export async function decrypt(key, value) {
+  let iv = value.slice(0, 16);
+  let ciphertext = value.slice(16);
+
+  const algorithm = {
+    name: 'AES-GCM',
+    iv: iv
+  }
+
+  let plaintext = await crypto.subtle.decrypt(algorithm, key, ciphertext);
+  plaintext = arrayBufferToString(plaintext);
+  return plaintext;
 }
 
-export async function signRandoms(privateKey, random1, random2) {
-  let randoms = new Uint8Array(random1.length + random2.length);
-  if(random1 > random2) {
-    randoms.set(random1);
-    randoms.set(random2, random1.length)
-  } else {
-    randoms.set(random2);
-    randoms.set(random1, random2.length)
-  }
 
-  let algorithm = {
-    name: 'ECDSA',
-    hash: 'SHA-512'
-  }
-  return await crypto.subtle.sign(algorithm, privateKey, randoms.buffer);
+
+// Uint8Array ----> HexString
+export function uint8ArrayToHexString(u8array) {
+  let str = "";
+  u8array.forEach((value) => {
+    let hexValue = value.toString(16);
+    while (hexValue.length < 2) hexValue = '0' + hexValue;
+    str += hexValue
+  });
+  return str;
 }
 
-export async function verifySignature(rawPublicKey, signature, random1, random2) {
-  let randoms = new Uint8Array(random1.length + random2.length);
-  if(random1 > random2) {
-    randoms.set(random1);
-    randoms.set(random2, random1.length)
-  } else {
-    randoms.set(random2);
-    randoms.set(random1, random2.length)
-  }
+// Uint8Array <---- HexString
+export function hexStringToUint8Array(hexString) {
+  return Uint8Array.from(hexString.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
+}
 
-  let algorithmKey = {
-    name: 'ECDSA',
-    namedCurve: 'P-256'
-  }
-  let publicKey = await crypto.subtle.importKey('raw', rawPublicKey, algorithmKey, true, ['verify']);
+// ArrayBuffer ----> String
+export function arrayBufferToString(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
 
-  let algorithVerify = {
-    name: 'ECDSA',
-    hash: 'SHA-512'
+// ArrayBuffer <---- String
+export function stringToArrayBuffer(str) {
+  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var bufView = new Uint16Array(buf);
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
   }
-  return await crypto.subtle.verify(algorithVerify, publicKey, signature, randoms);
+  return buf;
 }
